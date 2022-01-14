baf7c5530132f3e6eb4b30953d6c6da229b4d488
Nathan Nunes <nathannunes@pop-os.localdomain>
2022-01-14 18:37:03 -0300

    fix: header - remove importacao incorreta

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Logo from '../components/Logo';
 import Login from '../components/Login';
-import Header from '../components/Header';
 
 export default class Home extends Component {
   render() {
