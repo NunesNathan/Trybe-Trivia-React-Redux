5a9d1d706983a7d97da72492a658bf049c0b7bc1
Renata <renatamagno@live.com>
2022-01-18 15:50:42 -0300

    feat: feeedback - implementa mensagem de feedback

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import FeedbackMessage from '../components/FeedbackMessage';
 import Header from '../components/Header';
 
 export default class Feedback extends Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <FeedbackMessage />
+      </>
     );
   }
 }
