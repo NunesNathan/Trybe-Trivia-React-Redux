f773f7173fad660ab096aae507d4709da0055602
Renata <renatamagno@live.com>
2022-01-18 16:39:13 -0300

    feat: feedback - implementa botão de jogar novamente

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
+import PropType from 'prop-types';
+import Button from '../components/Button';
 import FeedbackInfo from '../components/FeedbackInfo';
 import FeedbackMessage from '../components/FeedbackMessage';
 import Header from '../components/Header';
 
 export default class Feedback extends Component {
+  playAgain = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <>
         <Header />
         <FeedbackMessage />
         <FeedbackInfo />
+        <Button
+          text="Play Again"
+          test="play-again"
+          onClick={ this.playAgain }
+          disabled={ false }
+        />
       </>
     );
   }
 }
+
+Feedback.propTypes = {
+  history: PropType.func.isRequired,
+};
